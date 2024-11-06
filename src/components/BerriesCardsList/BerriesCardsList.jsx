f08d9a0efbd9f56234eac7ecd0fb671fe6e8f002
volodymyr-martynov <v.v.martynov.work@gmail.com>
2024-11-07 01:58:40 +0200

    add empty state for card list

--- src/components/BerriesCardsList/BerriesCardsList.jsx
+++ src/components/BerriesCardsList/BerriesCardsList.jsx
@@ -2,6 +2,10 @@ import { BerryCard } from "../BerryCard";
 import styles from "./BerriesCardsList.module.css";
 
 export default function BerriesCardsList({ items }) {
+  if (!items.length) {
+    return <h3>There is no results for your query.</h3>;
+  }
+
   return (
     <div className={styles.list}>
       {items.map((item) => (
